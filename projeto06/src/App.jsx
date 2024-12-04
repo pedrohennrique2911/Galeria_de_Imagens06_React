4b4f467b806a892802c9a3cb2892bffc7051f3e7
pedrohennrique2911 <162355489+pedrohennrique2911@users.noreply.github.com>
2024-12-04 10:53:09 -0300

    Projeto06

---
+++ projeto06/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import GaleriaDeImagens from './components/Galeria'
+
+function App() {
+ 
+
+  return (
+    <>
+      <GaleriaDeImagens/>
+    </>
+  )
+}
+
+export default App
